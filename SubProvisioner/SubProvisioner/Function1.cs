311c9dd4bac9ccc46807a55cbd98391cfdb28d68
Andrew <kravcenko.andrejs@gmail.com>
2018-09-19 09:13:04 +0300

    comment on built-in roles

--- SubProvisioner/SubProvisioner/Function1.cs
+++ SubProvisioner/SubProvisioner/Function1.cs
@@ -93,6 +93,8 @@ namespace SubProvisioner
             {
                properties = new RbacProperties
                {
+                   //contributor - 	b24988ac-6180-42a0-ab88-20f7382dd24c
+                   //owner - 8e3af657-a8ff-443c-a75c-2fe8c4bcb635
                    roleDefinitionId = $"/subscriptions/{body.SubscriptionID}/providers/Microsoft.Authorization/roleDefinitions/b24988ac-6180-42a0-ab88-20f7382dd24c",
                    principalId = user.value[0].id.ToString()
                }
